ef9a9e5c836f2b511fc46bb15d6ad5036cda979e
hacker-flavio <theflavioherrera@gmail.com>
2023-08-21 11:54:05 -0700

    patched errors

--- button-switch.ino
+++ button-switch.ino
@@ -3,16 +3,16 @@ int buttonTwoPin = 9;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(buttonOnePin, INPUT_PULLUP)
-  pinMode(buttonOneTwo, INPUT_PULLUP)
+  pinMode(buttonOnePin, INPUT_PULLUP);
+  pinMode(buttonTwoPin, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(digitalRead(buttonOnePin) == LOW){
-     client.println("button one pressed");
+     Serial.println("button one pressed");
   }
   if(digitalRead(buttonTwoPin) == LOW){
-     client.println("button two pressed");
+     Serial.println("button two pressed");
   }
 }
